731f83ceb9998e0d6caa791458ca4380ad481af8
JacobRawling <mrjacob3@gmail.com>
2018-02-02 15:26:04 +0000

    Renamed truth to friend and removed detector_/reco_ prefix

--- CreateCSV/Root2CSV.py
+++ CreateCSV/Root2CSV.py
@@ -28,12 +28,12 @@ class TupleCSVConverter:
     def __init__(
             self,
             input_file,
-            detector_tuple_name,
-            truth_tuple_name,
+            tuple_name,
+            friend_tuple_name,
             output_folder,
             cuts = [],
-            truth_variables    = [],
-            detector_variables = [],
+            variables = [],
+            friend_variables    = [],
             verbosity = 1,
             create_custom_header= None,
             add_custom_variables= None,
@@ -44,27 +44,27 @@ class TupleCSVConverter:
             Converts a TTree to a csv file 
 
             input_file:           Root file input path 
-            detector_tuple_name:  name of the first tuple in the root file 
-            truth_tuple_name:     name of a second tuple in the file
+            tuple_name:  name of the first tuple in the root file 
+            friend_tuple_name:     name of a second tuple in the file
             output_folder:        location that the csv will be saved to
             cuts:                 A list of cuts for the reco events 
-            truth_variables:      A list of strings that can be evaluate upon the truth tuple
-            detector_variables:   A list of strings that can be evaluated upon the detector tuple
+            friend_variables:      A list of strings that can be evaluate upon the truth tuple
+            variables:   A list of strings that can be evaluated upon the detector tuple
             verbosity:            How much output this tool will display 
             create_custom_header: Function that can be override ti create a custom set of headers, must return an array of 
                                   strings
-            add_custom_variables: Function that takes reco_tree and truth_tree as inputs, and returns an array of numbers
+            add_custom_variables: Function that takes tree and friend_tree as inputs, and returns an array of numbers
                                   that will be saved to the csv file 
         """
         self.input_file          = input_file
-        self.detector_tuple_name = detector_tuple_name
-        self.truth_tuple_name    = truth_tuple_name
+        self.tuple_name = tuple_name
+        self.friend_tuple_name    = friend_tuple_name
         self.output_folder       = output_folder
         self.weight              = weight
         self.index_variable      = index_variable
         self.verbosity           = verbosity
-        self.truth_variables     = truth_variables
-        self.detector_variables  = detector_variables
+        self.friend_variables     = friend_variables
+        self.variables  = variables
         # Allow for cuts given in a ROOT::TTreeFormula expression style
         for cut in cuts:
             self.weight += "*(" + cut +  ")"
@@ -85,22 +85,22 @@ class TupleCSVConverter:
             headers = self.create_custom_header()
             for header in headers:
                 self.out_file.write(header)
-                if header != headers[-1] or len(self.truth_variables) > 0 or len(self.detector_variables) > 0:
+                if header != headers[-1] or len(self.friend_variables) > 0 or len(self.variables) > 0:
                     self.out_file.write(", ")
 
         # Examine truth variables if we are looking at this tuple
         if self.index_variable != None:
-            for var in self.truth_variables:
+            for var in self.friend_variables:
                 self.out_file.write(clean_string(var) )
                 # Add a comma iff we are not the last item on this line 
-                if var != self.truth_variables[-1] or len(self.detector_variables) > 0:
+                if var != self.friend_variables[-1] or len(self.variables) > 0:
                     self.out_file.write(", ")
 
         # Examine reconstruction level variables 
-        for var in self.detector_variables:
+        for var in self.variables:
             self.out_file.write(clean_string(var))
             # Add a comma iff we are not the last item on this line 
-            if var != self.detector_variables[-1]:
+            if var != self.variables[-1]:
                 self.out_file.write(", ")
 
         self.out_file.write("\n")
@@ -121,37 +121,37 @@ class TupleCSVConverter:
         in_file = open_file(self.input_file, "READ")
 
         # Get the tuples and synchronise them across eventNubers
-        reco_tree  = in_file.Get(self.detector_tuple_name)
+        tree  = in_file.Get(self.tuple_name)
 
         # Only read the truth tree if it's defined
         if self.index_variable != None:
-            truth_tree = in_file.Get(self.truth_tuple_name)
+            friend_tree = in_file.Get(self.friend_tuple_name)
         else:
-            truth_tree = None
+            friend_tree = None
 
         # Very costly for large files
         if self.index_variable != None:
-            reco_tree.BuildIndex(self.index_variable)
-            truth_tree.BuildIndex(self.index_variable)
+            tree.BuildIndex(self.index_variable)
+            friend_tree.BuildIndex(self.index_variable)
 
-        # Construct the weight based upon the reco_cuts:
-        weight_formula   = r.TTreeFormula("weight_formula", self.weight, reco_tree)
+        # Construct the weight based upon the cuts:
+        weight_formula   = r.TTreeFormula("weight_formula", self.weight, tree)
 
         # Create TTRee::Formulas for variables given as contsructors to save 
-        reco_formulae = []
-        for var in self.detector_variables:
-            reco_formulae.append(r.TTreeFormula(var, var, reco_tree))
+        formulae = []
+        for var in self.variables:
+            formulae.append(r.TTreeFormula(var, var, tree))
 
-        truth_formulae = []
+        friend_formulae = []
         if self.index_variable != None:
-           for var in self.truth_variables:
-               reco_formulae.append(r.TTreeFormula(var, var, truth_tree))
+           for var in self.friend_variables:
+               formulae.append(r.TTreeFormula(var, var, friend_tree))
 
-        for event in reco_tree:
-            index = reco_tree.GetEntryNumberWithIndex(event.eventNumber)
+        for event in tree:
+            index = tree.GetEntryNumberWithIndex(event.eventNumber)
             # Also read the truth level variable if we are trying to synchronise across two trees
             if self.index_variable != None:
-                truth_tree.GetEntry(index)
+                friend_tree.GetEntry(index)
 
             # Reject events that fail the cuts
             weight  = weight_formula.EvalInstance()
@@ -160,23 +160,24 @@ class TupleCSVConverter:
 
             # Also allow for a more complicated analysis of eents 
             if self.add_custom_variables != None:
-                custom_vars = self.add_custom_variables(event, truth_tree)
+                custom_vars = self.add_custom_variables(event, friend_tree)
                 # Save these to the file 
                 for var in custom_vars:
                    self.out_file.write(str(var))
-                   if var != custom_vars[-1] or len(truth_formulae) > 0 or len(reco_formulae) > 0:
+                   if var != custom_vars[-1] or len(friend_formulae) > 0 or len(formulae) > 0:
                        self.out_file.write(",")
 
+            # Save the friend tree variables first 
             if self.index_variable != None:
-                for formula in truth_formulae:
+                for formula in friend_formulae:
                     self.out_file.write( str(formula.EvalInstance()))
-                    if formula != truth_formulae[-1] or len(reco_formulae) > 0:
+                    if formula != friend_formulae[-1] or len(formulae) > 0:
                         self.out_file.write( ", ")
 
-
-            for formula in reco_formulae:
+            # Now save the main TTrees variables that we desire
+            for formula in formulae:
                 self.out_file.write( str(formula.EvalInstance()))
-                if formula != reco_formulae[-1]:
+                if formula != formulae[-1]:
                     self.out_file.write( ", ") 
             self.out_file.write("\n") 
 
